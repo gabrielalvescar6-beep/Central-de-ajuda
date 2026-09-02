f9bf499a5718513ee1a7a4d70cf35eeb0cac9472
Dark_404 <gabrielalvescar6@gmail.com>
2026-09-02 16:34:25 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,72 @@
+function Ligar(){
+
+
+switch (document.getElementById("departamento").value){
+
+
+case "1":
+    document.getElementById("resultado").innerHTML = "190";
+    break;
+    
+case "2":
+    document.getElementById("resultado").innerHTML = "193";
+    break;
+
+
+case "3":
+    document.getElementById("resultado").innerHTML = "192";
+    break;
+    
+case "4":
+    document.getElementById("resultado").innerHTML = "199";
+    break;
+    
+case "5":
+    document.getElementById("resultado").innerHTML = "(21)2332-39-24";
+    break;
+    
+case "6":
+    document.getElementById("resultado").innerHTML = "(21)2253-1177";
+    break;
+    
+case "7":
+    document.getElementById("resultado").innerHTML = "180";
+    break;
+    
+case "8":
+    document.getElementById("resultado").innerHTML = "100";
+    break;
+    
+case "9":
+    document.getElementById("resultado").innerHTML = "151";
+    break;
+    
+case "10":
+    document.getElementById("resultado").innerHTML = "(21) 2332-8611";
+    break;
+
+case "11":
+    document.getElementById("resultado").innerHTML = "(21) 3460-4040";
+    break;
+
+    
+case "12":
+    document.getElementById("resultado").innerHTML = "193";
+    break;
+
+    
+case "13":
+    document.getElementById("resultado").innerHTML = "(21) 2334-7910";
+    break;
+
+    
+case "0":
+    document.getElementById("resultado").innerHTML = "Seleção Inválida";
+    break;
+
+
+}
+
+
+
+}
